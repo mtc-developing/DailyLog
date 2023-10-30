ff4360e94a3b55aed481c4fcace7a277752ebc67
unknown <mtc.developing@gmail.com>
2023-10-30 00:20:54 -0400

    got the title animatoin for letters moving independentl;y

--- DailyLog/src/Title/Title.jsx
+++ DailyLog/src/Title/Title.jsx
@@ -2,21 +2,18 @@ import React from "react";
 
 import styles from "./Title.module.css"
 
-const space = "poop";
-
 function Title(props) {
     return (
         <div className={styles["title-container"]}>
             <div className={styles["main-title-container"]}>
                 <span className={styles.span}>D</span>
-                <span className={styles.span}>A</span>
-                <span className={styles.span}>I</span>
+                <span className={styles.span}>a</span>
+                <span className={styles.span}>i</span>
+                <span className={styles.span}>l</span>
+                <span className={styles.span}>y</span>
                 <span className={styles.span}>L</span>
-                <span className={styles.span}>Y</span>
-                <span className={styles.span}></span>
-                <span className={styles.span}>L</span>
-                <span className={styles.span}>O</span>
-                <span className={styles.span}>G</span>
+                <span className={styles.span}>o</span>
+                <span className={styles.span}>g</span>
             </div>
             <h2>a moble app by Cain Creative</h2>
         </div>

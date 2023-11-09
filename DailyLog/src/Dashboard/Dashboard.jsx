8de0990a1ece178127f7df98bf9b610866a7bf89
unknown <mtc.developing@gmail.com>
2023-11-08 19:21:02 -0500

    cleanup/alignment

--- DailyLog/src/Dashboard/Dashboard.jsx
+++ DailyLog/src/Dashboard/Dashboard.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 
-import styles from "./Dashboard.module.css"
+import styles from "./Dashboard.module.css";
 
 function Dashboard() {
 
     return (
         <div className={styles.dashboard}>
             <p>Testing Dashboard</p>
-            <p>8:32pm</p>
+            <span className={styles.time}>
+                <span>8</span>
+                <span className={styles.colon}>:</span>
+                <span>34</span>
+                <span>pm</span>
+            </span>
             <p>Monday</p>
         </div>
     )

0463146954fcc44963c16cf611b1aa9444b699ac
unknown <mtc.developing@gmail.com>
2023-11-15 21:38:01 -0500

    made a main overlay for other parts of the app but decided the settings bar can have its own entrance as it is unique in nature

--- DailyLog/src/App.jsx
+++ DailyLog/src/App.jsx
@@ -4,6 +4,7 @@ import Title from './Title/Title'
 import MainBackground from './MainBackground/MainBackground'
 import Homepage from './Homepage/Homepage.jsx'
 import Dashboard from "./Dashboard/Dashboard.jsx"
+import MainOverlay from './UIComponents/MainOverlay/MainOverlay.jsx'
 
 import styles from './App.module.css'
 
@@ -11,7 +12,7 @@ function App() {
   const [introFinished, setIntroFinished] = useState(false);
   const [liveTime, setLiveTime] = useState(new Date());
   const [count, setCount] = useState(0);
-
+  const [activeMainOverlay, setActiveMainOverlay] = useState(true);
   useEffect(() => {
     setTimeout(function () {
       setIntroFinished(true)
@@ -28,14 +29,30 @@ function App() {
     return () => clearInterval(interval)
   }, [count])
 
+  function toggleMainOverlay() {
+    if (activeMainOverlay) {
+      setActiveMainOverlay(false)
+    }
+    if (!activeMainOverlay) {
+      setActiveMainOverlay(true)
+    }
+  }
+
   return (
     <>
       <MainBackground />
+
+      {/* THIS IS THE MAIN OVERLAY FOR POPUPS OR COPOPNENTS THAT REQUIRE PRECEDENCE IN DISPLAY */}
+      {activeMainOverlay && <MainOverlay />}
+
+      {/* THIS IS WHERE THE INTRO ANIMATIONS LIVE */}
       {!introFinished && <Title />}
+
+      {/* THIS IS THE MAIN CONTENT OF THE APP. THE HOMEPAGE */}
       {introFinished && 
       <div className={styles["app-container"]}>
         <Dashboard liveTime={liveTime} />
-        <Homepage />
+        <Homepage toggleMainOverlay={toggleMainOverlay} />
       </div>
       }
       

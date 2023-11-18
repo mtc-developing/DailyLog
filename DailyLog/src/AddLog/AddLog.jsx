3cbfbf3930c6b793cfac412918fc00eaa11a13da
unknown <mtc.developing@gmail.com>
2023-11-18 02:06:03 -0500

    added overlay tab cvustom ui components to the newly created add log section, with click functionality for opening and closing the add log section

--- DailyLog/src/AddLog/AddLog.jsx
+++ DailyLog/src/AddLog/AddLog.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 
 // MY CUSTOM UI COMPONENTs
-import MainOverlay from "../UIComponents/MainOverlay/MainOverlay.jsx"
+import MainOverlay from "../UIComponents/MainOverlay/MainOverlay.jsx";
+import OverlayTab from '../UIComponents/OverlayTab/OverlayTab.jsx';
 
 // CUSTOM STYLE FILES IMPORT
 import styles from "./AddLog.module.css"
@@ -10,7 +11,11 @@ function AddLog(props) {
     
     return (
         <MainOverlay>
-            <div className={styles["add-log-container"]}>testing add log</div>
+            <div className={styles["add-log-container"]}>
+                <OverlayTab text="Add a Log" />
+                <div className={styles["add-log-body"]}>Add Log Body</div>
+                <OverlayTab text="X" onClick={props.toggleAddLog} />
+            </div>
         </MainOverlay>
     )
 }

0de73ee16b6f8c9af4c339594b2fad997c0de182
unknown <mtc.developing@gmail.com>
2024-09-29 18:14:06 -0400

    implemented custom num pad with button functionality urrently displaying one at a time, as opposed to combining them. up next!

--- DailyLog/src/AddLog/NumForm.jsx
+++ DailyLog/src/AddLog/NumForm.jsx
@@ -7,23 +7,30 @@ import styles from './NumForm.module.css'
 
 function NumForm(props) {
 
-    const [time, setTime] = useState("")
+    const [time, setTime] = useState("4:20pm")
+
+    function clickHandler(value) {
+        setTime(value)
+    }
     
     return (
         <div className={styles.NumForm}>
-            <div>0</div>
-            <div>1</div>
-            <div>2</div>
-            <div>3</div>
-            <div>4</div>
-            <div>5</div>
-            <div>6</div>
-            <div>7</div>
-            <div>8</div>
-            <div>9</div>
-            <div>AM</div>
-            <div>PM</div>
-            <div>clear</div>
+            <div onClick={() =>clickHandler("0")}>0</div>
+            <div onClick={() =>clickHandler("1")}>1</div>
+            <div onClick={() =>clickHandler("2")}>2</div>
+            <div onClick={() =>clickHandler("3")}>3</div>
+            <div onClick={() =>clickHandler("4")}>4</div>
+            <div onClick={() =>clickHandler("5")}>5</div>
+            <div onClick={() =>clickHandler("6")}>6</div>
+            <div onClick={() =>clickHandler("7")}>7</div>
+            <div onClick={() =>clickHandler("8")}>8</div>
+            <div onClick={() =>clickHandler("9")}>9</div>
+            <div onClick={() =>clickHandler("am")}>AM</div>
+            <div onClick={() =>clickHandler("pm")}>PM</div>
+            <div onClick={() =>clickHandler(":")}>:</div>
+            <div onClick={() =>clickHandler("")}>cl</div>
+            <div onClick={() =>clickHandler("add")}>ADD</div>
+            <div value="" className={styles["time-display"]}> {time} </div>
         </div>
     )
 }

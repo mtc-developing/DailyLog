4d1eb9fef91aca4e4b2fd9abe90d392e96ded242
unknown <mtc.developing@gmail.com>
2024-09-29 16:55:40 -0400

    enabled change of form to switch to a num pad for the time start time end inquries

--- DailyLog/src/AddLog/AddLog.jsx
+++ DailyLog/src/AddLog/AddLog.jsx
@@ -10,7 +10,7 @@ import NumForm from './NumForm.jsx';
 import styles from "./AddLog.module.css"
 
 // ATTEMPTING TO USE A VAR TO HOLD INMFORMATION FOR WHICH LOG FORM QUESTION WE ARE ON
-const listOfTitleText = ["Enter a title for your log",  "What did this task require you to do", "What time did you start this task" , "What time did you finish this task", "How many tallys would you like to add?"];
+const listOfTitleText = ["Enter a title for your log",  "What did this task require you to do", "What time did you start this task" , "What time did you finish this task"];
 const listOfTitles = ["Log Title", "Description", "Start Time", "End Time"];
 
 var listOfKeys = ["title", "description", "start_time", "end_time"];
@@ -43,10 +43,12 @@ function AddLog(props) {
             setLogData(rawData)
         }
 
-        // need a function to handle the change in iteration to change the form that pops up
-        // as well as a function for calculating the time  it took to completeb the task
+        if (iteration >= 1 && !numFormActive) {
+            setNumFormActive(true)
+        }
         
-        setIteration(iteration+1);
+        setIteration(iteration + 1);
+
         if (iteration >= listOfKeys.length - 1) {
             // ADD THE DATE AT THE VERY END RIGHT HERE
             logData.weekday = dayOfWeek;

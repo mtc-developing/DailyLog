e3794e5aadfe1292c9dfe5004458aa9811dddf58
unknown <mtc.developing@gmail.com>
2024-10-04 21:28:03 -0400

    fixed directions loop bug

--- DailyLog/src/Settings/Settings.jsx
+++ DailyLog/src/Settings/Settings.jsx
@@ -6,16 +6,16 @@ import MainOverlay from '../UIComponents/MainOverlay/MainOverlay.jsx';
 // CUSTOM STLYE IMPORTS
 import styles from './Settings.module.css';
 
-const TEXT_FILLERS = ["You come here often?", "Did it hurt?", "When you fell from heaven?", "I lost my number, can I have yours?", "Hahaha ok enough fucking around...", "Seriously, go use the app", "GO USE THE APP!!", "THIS ISNT FUNNY ANYMORE", "You know what, you're going to regret this...", "ha", "hahaha....", "HAHAHAHAHAHAHAHAHA"]
-
-var text = "What's up you sexy little bitch";
+const listOfDirections = ["If you struggle being productive with to-do lists, DailyLog aims to assist those who prefer an action first method that allows you to keep track of each would have been list item.", "Track the time it took to accomplish, see when you are at your most productive", 'Stay on task by refreshing your log, which will send an email summarizing what you accomplished', "log just so you can track your progress, and pat yourself on the back for your efforts", 'log so you can win a petty debate/argument with a friend or foe', 'log to document an incredible vacation, or an inspiring epiphony']
 
 function Settings(props) {
 
     const [emailInput, setEmailInput] = useState(false)
     const [inputValue, setInputValue] = useState("")
     const [openingPage, setOpeningPage] = useState(true)
-    const [textCount, setTextCount] = useState(0);
+    const [directions, setDirections] = useState(false)
+    const [directionsCount, setDirectionsCount] = useState(0)
+    const [text, setText] = useState(listOfDirections[0])
     
     const mainInput = useRef(null)
 
@@ -42,30 +42,38 @@ function Settings(props) {
     function addEmailHandler() {
         setOpeningPage(false);
         setEmailInput(true);
-        text = "When you refresh your logs, dailyLog will send an email to this address with your daily/weekly logs."
+        setText("When you refresh your logs, dailyLog will send an email to this address with your daily/weekly logs.")
     }
     function viewDirectionsHandler() {
         setOpeningPage(false);
+        setDirections(true)
         if (emailInput) {
             setEmailInput(false)
         };
-        text="If you struggle being productive with to-do lists, DailyLog aims to assist those who prefer an action first method that allows you to keep track of each would have been list item."
+        setDirectionsCount(0)
+        setText(listOfDirections[directionsCount])
+        
     }
     function forceRefreshHandler() {
         setOpeningPage(false);
         if (emailInput) {
             setEmailInput(false);
         };
-        text="Forcing a refresh will send all of your current logs to your email address if provided, as well as remove these logs from your local storage. Continue?"
+        setText("Forcing a refresh will send all of your current logs to your email address if provided, as well as remove these logs from your local storage. Continue?")
     }
 
     function buttonHandler() {
-        if (openingPage) {
-            setTextCount(textCount+1)
-            text = TEXT_FILLERS[textCount];
-            
+        if (directions && directionsCount >= listOfDirections.length-1) {
+            setText(listOfDirections[0])
+            setDirectionsCount(0)
             return
-        };
+        }
+        if (directions && directionsCount<=listOfDirections.length) {
+            console.log(directionsCount)
+            setText(listOfDirections[directionsCount + 1])
+            setDirectionsCount(directionsCount + 1)
+            return
+        }
         if (emailInput) {
             localStorage.setItem("userEmail", inputValue);
         };
@@ -88,7 +96,7 @@ function Settings(props) {
                 <div>{text}</div>
                 {/* we will pout a conditional here for email input */}
                 {emailInput && <input type="text" onChange={inputValueHandler} ref={mainInput} className={styles.input}></input>}
-                <div className={styles.button} onClick={buttonHandler}>OK</div>
+                {!openingPage && <div className={styles.button} onClick={buttonHandler}>OK</div>}
             </div>
         </MainOverlay>
     )
